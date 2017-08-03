8f6c54c25f7102a555caeb3106c254e8d2b20662
Mattias <mattiasparandeh@hotmail.com>
2017-08-03 13:27:16 -0400

    My first codea project

---
+++ MattiasGlobalVariables/MattiasGlobalVariables/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  Mattias Global Variables
+//
+//  Created by Mattias on Thursday, August 3, 2017
+//  Copyright (c) Mattias. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class StandaloneCodeaViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) StandaloneCodeaViewController *viewController;
+
+@end
